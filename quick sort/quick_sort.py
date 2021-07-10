8cee3ed27cfd85e9fabcd13ecdcf4c4deb92eac0
yuyeon329 <peachberryjr@gmail.com>
2021-07-10 17:39:02 +0900

    Quick Sort update

--- quick sort/quick_sort.py
+++ quick sort/quick_sort.py
@@ -1,12 +1,13 @@
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
-
+    #피벗 설정
     pivot = arr[len(arr)//2]
-    smaller_arr, equal_arr, bigger_arr = [], [], []
-    print("arr ", arr)
-    print("pivot ", pivot)
 
+    #분할된 데이터를 담을 list
+    smaller_arr, equal_arr, bigger_arr = [], [], []
+
+    #pivot과 비교해 비교 결과에 따라 list에 넣어줌
     for i in arr:
         if i<pivot:
             smaller_arr.append(i)
@@ -14,6 +15,8 @@ def quick_sort(arr):
             bigger_arr.append(i)
         else:
             equal_arr.append(i)
+
+    #재귀적으로 호출하여 분할 진행
     return quick_sort(smaller_arr) + quick_sort(equal_arr) + quick_sort(bigger_arr)
 
 
